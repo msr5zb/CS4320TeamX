446259b9fa29de4c9a765c09b44776d4f6b72123
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-19 14:10:12 -0600

    made the panel apart of master layout page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -52,8 +52,26 @@
 			</div>
 		</div>
 	</nav>
-
-	@yield('content')
+	<div class="container">
+		@if (!Auth::guest())
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+		@endif
+				@yield('content')
+				@if ( $errors->any() )
+					<ul class="alert alert-danger">
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				@endif
+		@if (!Auth::guest())
+				</div>
+			</div>
+		</div>
+		@endif
+	</div>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
