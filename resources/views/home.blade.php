caea57c108d612348eb4b4d41f77188591cfe86a
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-19 14:17:59 -0600

    moved the panel to app.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,12 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					{!! Form::open(array('url' => '/testScore', 'method' => 'get')) !!}
-						<div class="form-group">
-							{!! Form::submit('Create Access Application', ['class' => 'btn btn-primary form-control']) !!}
-						</div>
-					{!! Form::close() !!}
-				</div>
-			</div>
-		</div>
+<div class="panel-heading">Home</div>
+<div class="panel-body">
+	{!! Form::open(array('url' => '/testScore', 'method' => 'get')) !!}
+	<div class="form-group">
+		{!! Form::submit('Create Access Application', ['class' => 'btn btn-primary form-control']) !!}
 	</div>
+	{!! Form::close() !!}
 </div>
 @endsection
