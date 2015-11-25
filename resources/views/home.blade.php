92e9a093455ae483469212792e9a775b3c39e910
Michael Rallo <msr5zb@mail.missouri.edu>
2015-11-25 06:44:53 +0000

    Added UI stuffs / Organized a few things

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
 @extends('app')
 
 @section('content')
-<div class="panel-heading">Home</div>
-<div class="panel-body">
-	{!! Form::open(array('url' => '/testScore', 'method' => 'get')) !!}
-	<div class="form-group">
-		{!! Form::submit('Create Access Application', ['class' => 'btn btn-primary form-control']) !!}
-	</div>
-	{!! Form::close() !!}
-</div>
+    
+<!--Tutorial Section! -->    
+                <section id="block-download " class="fancySection">
+                    <div class="page-header fancyHeader" id="test">
+                        <h1>Welcome, {{ Auth::user()->userSSO }}</h1>
+                    </div>
+                    <div id="siteTutContainer">
+                        <img src="{{ asset('/images/siteTutorial.png') }}" id="siteTut" />
+                    </div>
+                    
+                    <div class="panel-body">
+                        {!! Form::open(array('url' => '/testScore', 'method' => 'get')) !!}
+                        <div class="form-group">
+                            {!! Form::submit('Create Access Application', ['class' => 'btn btn-primary form-control']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                </div>
+                       
+                </section>  
 @endsection
