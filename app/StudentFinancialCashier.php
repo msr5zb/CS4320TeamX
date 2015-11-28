eac20dbb51f50fa0263a51d35b7e955d8cd67000
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 12:10:15 -0700

    Added PK, and turned off timestamps and incrementer

--- app/StudentFinancialCashier.php
+++ app/StudentFinancialCashier.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentFinancialCashier extends Model
 {
+	// Set the primary key
+	protected $primaryKey = 'requestId';
+
+	// Turn off the timestamps
+	public $timestamps = false;
+
+	// Turn off the auto incrementer
+	public $increments = false;
+
     //allow mass assignment for the following attributes
     protected $fillable = [
 	    'generalInquiryView',
