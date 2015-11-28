74cbb3149e2019a4f6159ea3ffb19243ebe250a6
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 11:58:04 -0700

    Added primaryKey, turned off timestamps and incrementer

--- app/StudentRecords.php
+++ app/StudentRecords.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentRecords extends Model
 {
+    // Set the primary Key 
+    protected $primaryKey = 'requestId';
+
+    // Turn off timestamps, since they are not included in the table
+    public $timestamps = false;
+
+    //Turn off incrementer, eloquent expects a variable to auto increment
+    public $increments = false;
+     
     //allow mass assignment for the following attributes
     protected $fillable = [
         'basicInquiryView',
