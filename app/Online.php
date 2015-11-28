b1dd97a2a2d956c85f598d57cbe2e10c38fd0dfa
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 12:16:16 -0700

    Turned off incrementer for this model

--- app/Online.php
+++ app/Online.php
@@ -10,8 +10,11 @@ class Online extends Model {
 	// Set the primary key
 	public $primaryKey = 'userSSO';
 
-	// Turn of automatic timestamps, we do not have these defined in the table
+	// Turn off automatic timestamps, we do not have these defined in the table
 	public $timestamps = false;
+
+	// Turn off incrementer, since eloquent assumes that there is at least one variable that increments
+	public $increments = false;
 
 	// Mass assignment allowed entities to table
 	protected $fillable = ['userSSO'];
