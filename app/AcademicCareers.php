7653f98028aead1b4e6b67a7a8c0815d71472eae
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 11:43:45 -0700

    Added primaryKey, turned off timestamps and incrementer

--- app/AcademicCareers.php
+++ app/AcademicCareers.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class AcademicCareers extends Model
 {
+	// Set the primaryKey
+	protected $primaryKey = 'requestId';
+
+	// Turn off timestamps
+	public $timestamps = false;
+
+	// Turn off increments
+	public $increments = false;
+
     //allow mass assignment for the following attributes
     protected $fillable = [
     	'ugrd',
