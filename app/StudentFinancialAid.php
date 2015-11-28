6c0d80c402dc308efcf3a383eed53fe83a845436
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 12:14:15 -0700

    Added PK, and turned of timestamps and incrementer

--- app/StudentFinancialAid.php
+++ app/StudentFinancialAid.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentFinancialAid extends Model
 {
+	// Set the primary Key
+	protected $primaryKey = 'requestId';
+
+	// Turn off timestamps, since they are not included in the table
+	public $timestamps = false;
+
+	// Turn off incrementer, since eloquent assumes each table has a variable that auto increments
+	public $increments = false;
+	
     //allow mass assignment for the following attributes
     protected $fillable = [
     	'cashView',
