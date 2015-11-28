89327ca840e25ab80f6a3b9625c60ff1666db427
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-28 12:04:39 -0700

    Added the mass assignment variables, added PK, and turned off timestamps and incrementer

--- app/Admissions.php
+++ app/Admissions.php
@@ -6,8 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admissions extends Model
 {
+	// Set the primary key 
+	protected $primaryKey = 'requestId';
+
+	// Turn off timestamps
+	public $timestamps = false;
+
+	// Turn off incrementer 
+	public $increments = false;
+
     //allow mass assignment for the following attributes
     protected $fillable = [
-    	''
+    	'act',
+    	'sat',
+    	'gre',
+    	'gmat',
+    	'tofel',
+    	'ielts',
+    	'lsat',
+    	'mcat',
+    	'ap',
+    	'clep',
+    	'ged',
+    	'millers',
+    	'prax',
+    	'plamu',
+    	'base'
     ];
 }
