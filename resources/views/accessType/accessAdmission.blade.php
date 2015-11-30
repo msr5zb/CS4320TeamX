6ffff6155cfc38da2ab733658ba74a71fbafe46e
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-11-29 19:48:25 -0600

    Added submit button

--- resources/views/accessType/accessAdmission.blade.php
+++ resources/views/accessType/accessAdmission.blade.php
@@ -10,31 +10,33 @@
     <div class="col-md-8">
       Check with test(s) access is to granted
     </div>
-    <div class="c0l-md-3">
-      {!! Form::checkbox('selectAll', 'selectAll', false, ['id' => 'selectAll']) !!} Access to ALL test scores
+    <div class="col-md-4">
+      {!! Form::checkbox('admissionsAccess[]', 'selectAll', false, ['id' => 'selectAll']) !!} Access to ALL test scores
     </div>
   </div>
   <div class="row">
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'act', false, ['class' => 'adAccessBoxes'])!!} ACT </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'sat', false, ['class' => 'adAccessBoxes'])!!} SAT </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'gre', false, ['class' => 'adAccessBoxes'])!!} GRE </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'gmat', false, ['class' => 'adAccessBoxes'])!!} GMAT </div>
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'tofel', false, ['class' => 'adAccessBoxes'])!!} TOEFL </div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'act', false, ['class' => 'adAccessBoxes'])!!} ACT </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'sat', false, ['class' => 'adAccessBoxes'])!!} SAT </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'gre', false, ['class' => 'adAccessBoxes'])!!} GRE </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'gmat', false, ['class' => 'adAccessBoxes'])!!} GMAT </div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'tofel', false, ['class' => 'adAccessBoxes'])!!} TOEFL </div>
   </div>
   <div class="row">
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'ielts', false, ['class' => 'adAccessBoxes'])!!} IELTS </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'lsat', false, ['class' => 'adAccessBoxes'])!!} LSAT </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'mcat', false, ['class' => 'adAccessBoxes'])!!} MCAT </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'ap', false, ['class' => 'adAccessBoxes'])!!} AP </div>
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'clep', false, ['class' => 'adAccessBoxes'])!!} CLEP </div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'ielts', false, ['class' => 'adAccessBoxes'])!!} IELTS </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'lsat', false, ['class' => 'adAccessBoxes'])!!} LSAT </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'mcat', false, ['class' => 'adAccessBoxes'])!!} MCAT </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'ap', false, ['class' => 'adAccessBoxes'])!!} AP </div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'clep', false, ['class' => 'adAccessBoxes'])!!} CLEP </div>
   </div>
   <div class="row">
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'ged', false, ['class' => 'adAccessBoxes'])!!} GED </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'millers', false, ['class' => 'adAccessBoxes'])!!} MILLERS </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'prax', false, ['class' => 'adAccessBoxes'])!!} PRAX </div>
-    <div class="col-md-2">{!! Form::checkbox('adAccess[]', 'plamu', false, ['class' => 'adAccessBoxes'])!!} PLA-MU</div>
-    <div class="col-md-3">{!! Form::checkbox('adAccess[]', 'base', false, ['class' => 'adAccessBoxes'])!!} BASE </div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'ged', false, ['class' => 'adAccessBoxes'])!!} GED </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'millers', false, ['class' => 'adAccessBoxes'])!!} MILLERS </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'prax', false, ['class' => 'adAccessBoxes'])!!} PRAX </div>
+    <div class="col-md-2">{!! Form::checkbox('admissionsAccess[]', 'plamu', false, ['class' => 'adAccessBoxes'])!!} PLA-MU</div>
+    <div class="col-md-3">{!! Form::checkbox('admissionsAccess[]', 'base', false, ['class' => 'adAccessBoxes'])!!} BASE </div>
   </div>
+  <br>
+   {!! Form::Submit('Next', ['class' => 'btn btn-primary form-control']) !!}
 </div>
 {!! Form::close() !!}
 </div>
