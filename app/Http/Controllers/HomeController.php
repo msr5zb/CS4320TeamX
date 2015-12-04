2eb8af3477c8d04d25f85eac8f4cc7af6f29d624
wdmpp5 <wdmpp5@mail.missouri.edu>
2015-12-04 11:02:42 -0600

    Initial unit testing
    Added unit tests for all table creations (good) and deletions (buggy,
    commented out)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,62 @@ class HomeController extends Controller {
 	 */
 	public function __construct()
 	{
+		//add authentication middleware to home controller
 		$this->middleware('auth');
+
+		//assert all tables can be created
+		assert( 'new App\About' == true );
+		assert( 'new App\AcademicCareers' == true );
+		assert( 'new App\Admissions' == true );
+		assert( 'new App\ChromePhp' == true );
+		assert( 'new App\FormInfo' == true );
+		assert( 'new App\Request' == true );
+		assert( 'new App\Reserved' == true );
+		assert( 'new App\StudentFinancialAid' == true );
+		assert( 'new App\StudentFinancialCashier' == true );
+		assert( 'new App\StudentRecords' == true );
+
+		/*assert all tables can be destroyed
+		$example = new \App\About;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\AcademicCareers;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\Admissions;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\ChromePhp;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\FormInfo;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\Request;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\Reserved;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\StudentFinancialAid;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\StudentFinancialCashier;
+		assert( $example->destroy() == true );
+		$example->destroy();
+
+		$example = new \App\StudentRecords;
+		assert( $example->destroy() == true );
+		$example->destroy();*/
+
 	}
 
 	/**
