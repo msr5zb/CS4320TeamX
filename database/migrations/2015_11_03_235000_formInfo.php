61af5e343763a5b1c4a3e69a6027298085c5b4f5
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-12-06 16:08:44 -0600

    Changed titled to migrate before request table

---
+++ database/migrations/2015_11_03_235000_formInfo.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FormInfo extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		 Schema::dropIfExists('form_info');
+        Schema::create('form_info', function (Blueprint $table) {
+            $table->increments('requestId');
+            $table->boolean('admissions')->default(0);
+            $table->boolean('studentRecords')->default(0);
+            $table->boolean('finanCashier')->default(0);
+            $table->boolean('finanAid')->default(0);
+            $table->boolean('reserved')->default(0);
+            $table->timestamps();
+        });
+	}
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		 Schema::drop('form_info');
+	}
+
+}
