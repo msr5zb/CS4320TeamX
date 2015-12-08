d4bd069af6adbd9b68b519e6ce4c7f24f315aff1
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-12-07 18:48:51 -0600

    Added complete column to migration

--- database/migrations/2015_11_03_235000_formInfo.php
+++ database/migrations/2015_11_03_235000_formInfo.php
@@ -16,11 +16,6 @@ class FormInfo extends Migration {
         Schema::create('form_info', function (Blueprint $table) {
             $table->increments('requestId');
             $table->boolean('complete')->default(0);
-            $table->boolean('admissions')->default(0);
-            $table->boolean('studentRecords')->default(0);
-            $table->boolean('finanCashier')->default(0);
-            $table->boolean('finanAid')->default(0);
-            $table->boolean('reserved')->default(0);
             $table->timestamps();
         });
 	}
